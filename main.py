3a8b3f875836565cd443063bf2a48f8e983a92c6
SaanviK04 <127449341+SaanviK04@users.noreply.github.com>
2023-03-09 17:14:30 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+
+menu_option = 1
+
+while menu_option != 3:
+    print('Menu')
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
+    print('')
+    print('Please enter an option:')
+    menu_option = int(input())
+
+    if menu_option == 1:
+        print('Please enter your password to encode:')
+        og_password = str(input())
+        encoded_password = ''
+        place_value = 0
+
+        while place_value < len(og_password):
+            encoded_password += str( int(og_password[place_value]) + 3)
+            place_value += 1
+        print('Your password has been encoded and stored!')
+        print('')
+
+
+
+
+
+
+
+
